2a9126280892c09416e1694e4df4751a6dd12c30
TheLuirter15 <or427578@uaeh.edu.mx>
2025-02-04 19:58:49 -0600

    Update 1.2 Programa 1.cpp

--- 1.2 Programa 1.cpp
+++ 1.2 Programa 1.cpp
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <iostream>
 #include <cctype>
 
@@ -26,7 +18,7 @@ string clasificarCadena(const string& cadena) {
     }
 
     if (tieneNumeros)
-        return "Número de tipo entero";
+        return "Número entero";
     if (tieneLetras)
         return "Palabra";
 
