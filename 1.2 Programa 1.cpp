72eab0517e67f01430d73f0d5ccbb3e659b37e86
TheLuirter15 <or427578@uaeh.edu.mx>
2025-02-04 19:58:05 -0600

    Add files via upload

--- 1.2 Programa 1.cpp
+++ 1.2 Programa 1.cpp
@@ -1,3 +1,11 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
 #include <iostream>
 #include <cctype>
 
@@ -18,7 +26,7 @@ string clasificarCadena(const string& cadena) {
     }
 
     if (tieneNumeros)
-        return "Número entero";
+        return "Número de tipo entero";
     if (tieneLetras)
         return "Palabra";
 
